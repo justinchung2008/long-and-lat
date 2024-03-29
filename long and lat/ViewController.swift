099c6144e04b583dc4e588eb9fb7e97ec1f6e9a2
Justin Chung <justinchung2026@cdnis.edu.hk>
2024-03-30 00:40:14 +0800

    Initial Commit

---
+++ long and lat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  long and lat
+//
+//  Created by Cheuk Yin Chung on 30/3/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
